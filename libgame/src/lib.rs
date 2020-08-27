0e0a96003a00e17bdb39f4f7b2b12ea62bf80a80
Spiros Makris <makris.spiros@gmail.com>
2020-08-27 10:05:14 +0300

    Setup gdnative for libgame

--- libgame/src/lib.rs
+++ libgame/src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use gdnative::prelude::*;
+
+fn init(_handle: InitHandle) {
+
 }
+
+godot_init!(init);
